dccca8cc845b67e185f34e7723a64d987bb5c412
Jonathan Lindqvist <jonathan.x.lindqvist@gmail.com>
2021-05-16 19:28:05 +0200

    Use selenium to open browser and take screenshot

--- autoclicker.py
+++ autoclicker.py
@@ -1,3 +1,13 @@
 import webbrowser
+from selenium import webdriver
+from time import sleep
 
-webbrowser.open("http://www.aimbooster.com/")
+# webbrowser.open("http://www.aimbooster.com/")
+
+driver = webdriver.Chrome()
+driver.get('https://www.python.org')
+sleep(1)
+
+driver.get_screenshot_as_file("screenshot.png")
+driver.quit()
+print("end...")
